0e35ee7e5d9428751bf60b2468925cafd1b4e2cd
BEN-127-25 <benedictjohnaventajado@gmail.com>
2025-07-28 17:12:06 +0800

    MASSAGE

---
+++ src/activity1/pkg2c/Activity12c.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity.pkg1;
+
+/**
+ *
+ * @author SCC_PC08
+ */
+public class Activity1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.printIn("Enter your name; ");
+        name = sc.nextline();
+        
+    }
+    
+}
